1a81fd56bde92afeb092630abb0332baff66b330
Robin Boucher <robin.boucher@epita.fr>
2021-08-14 23:58:24 +0200

    feat(main): add GET comics & POST scrap

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module freetchio-api
 go 1.16
 
 require (
-	github.com/gin-gonic/gin v1.7.2
+	github.com/gin-gonic/gin v1.7.3
 	golang.org/x/net v0.0.0-20210726213435-c6fcb2dbf985
 )
--- main.go
+++ main.go
@@ -6,17 +6,20 @@ func main() {
 	// coroutine that takes care of scrapping itch.io and creating the JSON files
 	go ScrapItchioEvery12Hours()
 
-	// set the API
+	// Set the API.
 	router := gin.Default()
 
 	router.GET("/game-assets", GetGameAssets)
 	router.GET("/books", GetBooks)
+	router.GET("/comics", GetComics)
 	router.GET("/tools", GetTools)
 	router.GET("/games", GetGames)
 	router.GET("/physical-games", GetPhysicalGames)
 	router.GET("/soundtracks", GetSoundtracks)
 	router.GET("/game-mods", GetGameMods)
 	router.GET("/misc", GetMisc)
+
+	router.POST("/scrap", StartScrap)
 
 	router.Run()
 }
