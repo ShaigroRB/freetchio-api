5184cb9b4373480eb81b8580bd50e72c66e81ebd
Robin Boucher <robin.boucher@epita.fr>
2021-08-15 00:02:05 +0200

    feat(main): refactor scrapItchio
    - Remove the notion of scrapping every 12 hours.
    - Remove the writing of json file.
    - Resulting items are now written using a WriteItems function given as
      parameter to scrapItchio.

--- scrap.go
+++ scrap.go
@@ -2,38 +2,25 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"time"
-
 	"freetchio-api/itch"
 )
 
-// writeJSONFile writes content into a JSON file.
-func writeJSONFile(content string, filename string) error {
-	jsonFilename := fmt.Sprintf("%s.json", filename)
-	contentBytes := []byte(content)
-	err := ioutil.WriteFile(jsonFilename, contentBytes, 0644)
-	return err
-}
+// WriteItemsFn is a common type for Create & Update of the ItemService interface
+type WriteItemsFn func(string, string) error
 
-// scrapItchio creates new JSON files for all the free on-sales items of itch.io.
-func scrapItchio() {
-	for _, category := range itch.Categories {
-		jsonString := itch.GetCategoryItemsAsJSON(category)
-		err := writeJSONFile(jsonString, string(category))
-
-		if err != nil {
-			fmt.Println(err)
-		}
+// writeValueForCategory writes value for a category
+func writeValueForCategory(category itch.Category, value string, fn WriteItemsFn) {
+	key := string(category)
+	err := fn(key, value)
+	if err != nil {
+		fmt.Println(err)
 	}
 }
 
-// ScrapItchioEvery12Hours creates new JSON files every 12 hours for all the free on-sale items of itch.io.
-func ScrapItchioEvery12Hours() {
-	// Create the JSON files for the first time
-	scrapItchio()
-
-	for range time.Tick(time.Hour * 12) {
-		scrapItchio()
+// scrapItchio creates new JSON files for all the free on-sales items of itch.io.
+func scrapItchio(fn WriteItemsFn) {
+	for _, category := range itch.Categories {
+		jsonString := itch.GetCategoryItemsAsJSON(category)
+		writeValueForCategory(category, jsonString, fn)
 	}
 }
